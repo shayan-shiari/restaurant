c73e74f51aea2430a7e159273c41db539e887526
shayan <shayanshiari@gmail.com>
2024-01-11 07:09:14 -0800

    menu-page added

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,17 +1,23 @@
+import { InferGetStaticPropsType, GetStaticProps } from "next";
 import axios from "axios";
 import React from "react";
+import { IMenu } from "@/types/menu.type";
 
-const Menu = () => {
-  return <div>as</div>;
+const Menu = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(data);
+
+  return <div>
+    <h2>Menu</h2>
+  </div>;
 };
 
 export default Menu;
 
-export function getStaticProps() {
-  const res = axios.get("http://localhost:4000/data");
-  console.log(res);
+export const getStaticProps: GetStaticProps<{ data: IMenu[] }> = async (
+  context
+) => {
+  const res = await axios.get("http://localhost:4000/data");
+  const data: IMenu[] = await res.data;
 
-  return {
-    props: {},
-  };
-}
+  return { props: { data }, revalidat: 10 };
+};
---
+++ src/types/menu.type.tsx
@@ -0,0 +1,10 @@
+export type IMenu = {
+  details: [];
+  discount: Number;
+  id: Number;
+  ingredients: [];
+  introduction: string;
+  name: string;
+  price: number;
+  recipe: [];
+}
