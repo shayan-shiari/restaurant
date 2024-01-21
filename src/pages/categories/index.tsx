9d4854e6ed6f24f7fb83d954a9f8c89d73c567fe
shayan <shayanshiari@gmail.com>
2024-01-21 07:07:59 -0800

    query extended

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { IMenu } from "@/types/menu.type";
 
-const Categories = () => {
+const Categories = ({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
   const [query, setQeury] = useState({
     difficulty: "",
@@ -58,3 +63,41 @@ const Categories = () => {
 };
 
 export default Categories;
+
+export const getServerSideProps: GetServerSideProps<{ data: IMenu[] }> = async (
+  context
+) => {
+  const {
+    query: { difficulty, time },
+  } = context;
+
+  const res = await axios.get("http://localhost:4000/data");
+  const data: IMenu[] = await res.data;
+  const filteredData = data.filter((item) => {
+    const difficultyResult = item.details.filter(
+      (item) => item.Difficulty && item.Difficulty === difficulty
+    );
+
+    const timeResult = item.details.filter((detail) => {
+      const cookingTime = detail["Cooking Time"] || "";
+      const [timeDetail] = cookingTime.split(" ");
+      if (time === "less" && timeDetail && +timeDetail <= 30) {
+        return detail;
+      } else if (time === "more" && +timeDetail > 30) {
+        return detail;
+      }
+    });
+
+    if (time && difficulty && timeResult.length && difficultyResult.length) {
+      return item;
+    } else if (!time && difficulty && difficultyResult.length) {
+      return item;
+    } else if (time && !difficulty && timeResult.length) {
+      return item;
+    }
+  });
+
+  return {
+    props: { data: filteredData },
+  };
+};
